05ef0d47bcf025ebd59f2381ac2b0af8e77a7982
Bharathbandi96 <bharathbandi96@gmail.com>
2019-10-01 10:42:02 +0530

    5 files modified

--- model.js
+++ model.js
@@ -1,11 +1,13 @@
-// function addItemToLocalStorage(){
-//   localStorage.setItem('myTodoItems', JSON.stringify(todos));
-// }
+<script src = 'controller.js'></script>
 
-// function renderItemsFromLocalStorage(){
-//   var todoItems = localStorage.getItem('myTodoItems');
-//   if(todoItems!=null){
-//   todos = JSON.parse(todoItems);
-//   }
-//   return todos;
-// }
+function addItemToLocalStorage(){
+  localStorage.setItem('myTodoItems', JSON.stringify(todos));
+}
+
+function renderItemsFromLocalStorage(){
+  var todoItems = localStorage.getItem('myTodoItems');
+  if(todoItems!=null){
+  todos = JSON.parse(todoItems);
+  }
+  return todos;
+}
