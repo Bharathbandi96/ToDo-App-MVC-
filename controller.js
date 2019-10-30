5ff473a4f9e1ee252bfca037608ee82071fe196c
Bharathbandi96 <bharathbandi96@gmail.com>
2019-10-30 09:28:52 +0530

    3 files modified

--- controller.js
+++ controller.js
@@ -1,255 +1,210 @@
 
-showInvalidStorageMessage();
-attachEventListner();
-createAddTaskButton();
-createAllTaskButton();
-createCompletedTaskButton();
-createPendingTaskButton();
-
-//
-function attachEventListner(){
-  document.getElementById('myInput').addEventListener('keypress',getItemOnEnter);
-  document.getElementById('selectStorage').addEventListener('change',getStorageItems);
+function createControllerInstance(){
+    return new Controller();
 }
 
-//createAddButtonAndSetEvent
-function createAddTaskButton(){
-  var button = document.createElement("BUTTON");
-  button.id = 'addButton';
-  button.appendChild(document.createTextNode("Add"));
-  appendAddButton(button);
-  button.addEventListener('click', getItemOnAddButtonClick);
-}
-//createAllTaskButtonAndSetEvent
-function createAllTaskButton(){
-  var button = document.createElement("BUTTON");
-  button.id = 'allTaskButton';
-  button.appendChild(document.createTextNode("All Tasks"));
-  appendTaskButtons(button);
-  button.addEventListener('click', getAllItems);
-}
-//createCompletedButtonAndSetEvent
-function createCompletedTaskButton(){
-  var button = document.createElement("BUTTON");
-  button.id = 'completedTaskButton';
-  button.appendChild(document.createTextNode("Completed"));
-  appendTaskButtons(button);
-  button.addEventListener('click', getCompletedItems);
-}
-//createPendingButtonAndSetEvent
-function createPendingTaskButton(){
-  var button = document.createElement("BUTTON");
-  button.id = 'pendingTaskButton';
-  button.appendChild(document.createTextNode("Pending"));
-  appendTaskButtons(button);
-  button.addEventListener('click', getPendingItems);
-}
-//
-function showInvalidStorageMessage(){
-  var storageMessage = 'Please select your required storage to store data...';
-  document.getElementById('displayArea').innerHTML = storageMessage;
-}
-//
-function getStorageType(){
-  return document.getElementById("selectStorage").value;
-}
+createControllerInstance().attachEventListner();
+createControllerInstance().showMessageOnInvalidStorage();
 
-//checkStorageTypeAndPerformTasks
-function getStorageItems(){
-  var selectedStorage = getStorageType();
-  if(selectedStorage === 'localStorage' || selectedStorage === 'sessionStorage'){
-    var storageData = getItemsFromStorage();
-    renderStorageData(storageData);
-  } else {
-    showInvalidStorageMessage();
-    displayItemsCount(0);
-  }
-}
+function Controller(){
 
-//displayItemsAndItsCount
-function renderStorageData(storageData){
-  clearDisplayArea(document.getElementById('displayArea'));
-  for(var i = 0; i < storageData.length; i++){
-    createDisplayItemInstance(storageData[i].id,storageData[i].name,storageData[i].status);
-  }
-  displayItemsCount(storageData.length);
-}
+  var self = this;
 
-//
-function createDisplayItemInstance(id,item,status){
-  return new DisplayItem(id,item,status).createItem();
-}
-//
-function createStorageManagerInstance(){
-  var selectedStorage = getStorageType();
-  if(selectedStorage === 'localStorage' || selectedStorage === 'sessionStorage'){
-    return new StorageManager(selectedStorage,'myTodoItems');
+  this.attachEventListner = function(){
+    document.getElementById('myInput').addEventListener('keypress',self.getItemOnEnter);
+    document.getElementById('selectStorage').addEventListener('change',self.tasksOnStorageType);
   }
-  else{
-    return '';
-  }
-}
-//
-function getItemsFromStorage(){
-   return createStorageManagerInstance().getData();
-  }
-//clearTaskDisplayArea
-function clearDisplayArea(displayAreaId){
-  displayAreaId.innerHTML = '';
-}
-//
-function getItemOnEnter() {
-  var enterKeyCode = 13;
-  var inputText = document.getElementById('myInput');
-  if (event.keyCode === enterKeyCode){
-  displayTask(inputText);
-  }
-}
-//
-function getItemOnAddButtonClick() {
-  var inputText = document.getElementById('myInput');
-  displayTask(inputText);
-}
 
-//tasksToPerformOnAddNewTask
-function displayTask(inputText){
-  var selectedStorage = getStorageType();  
-  if(inputText.value !== '' && (selectedStorage === 'localStorage' || selectedStorage === 'sessionStorage')){
-    getAllItems();
-    addItemToAnArray(inputText.value);
-    createDisplayItemInstance(Date.now(),inputText.value);
-    inputFieldReset(inputText);
-    displayItemsCount(getItemsFromStorage().length);
+  this.showMessageOnInvalidStorage = function(){
+    var storageMessage = 'Please select your required storage to store data...';
+    document.getElementById('displayArea').innerHTML = storageMessage;
   }
-}
-//
-function addItemToAnArray(item){
-  var storageData = getItemsFromStorage();
-  storageData.push({id:Date.now(),name:item,status:false});
-  addItemsToStorage(storageData);
-}
-//
-function addItemsToStorage(storageData){
-  var selectedStorage = getStorageType();
-  if(selectedStorage === 'localStorage' || selectedStorage === 'sessionStorage'){
-    createStorageManagerInstance().setData(storageData);
-  }
-}
-//
-function createAnItem(id,item,status){
-  var li = document.createElement("li");
-  li.id = id;
-  createCheckButton(li,status);
-  createTextContent(li,item);
-  createDeleteButton(li);
-  appendItemToList(li);
-}
-//createCheckButtonAndSetEvent
-function createCheckButton(li,status){
-  var span = document.createElement("SPAN");
-  span.id = "check";
-  // attachEventToCheckButton(span);
-  span.addEventListener('click',getItemToChangeStatus);
-  setClassValue(span,status);
-  li.appendChild(span);
-}
 
-//
-function getItemToChangeStatus(){
-  var storageData = getItemsFromStorage();
-  var item = this.parentElement.id;
-  this.classList.toggle('checked');
-  var currentItem = storageData.find(function(array){
-    return array.id === Number(item);
-  });
-  setStatusValue(this.classList.value,storageData.indexOf(currentItem));
-}
-//setStatusValueToStorage
-function setStatusValue(classValue,itemIndex){
-  var storageData = getItemsFromStorage();
-  (classValue === '') ? storageData[itemIndex].status = false : storageData[itemIndex].status = true;
-  addItemsToStorage(storageData);
-}
-//setItemClassValue
-function setClassValue(span,status){
-  if(status === true){
-    span.classList = 'checked';
+  this.getStorageType = function(){
+    return document.getElementById("selectStorage").value;
   }
-}
-//
-function createTextContent(li,item){
-  var span = document.createElement("SPAN");
-  span.appendChild(document.createTextNode(item));
-  li.appendChild(span);
-}
-//createDeleteButtonAndSetEvent
-function createDeleteButton(li){
-  var span = document.createElement("SPAN");
-  span.className = "close";
-  span.appendChild(document.createTextNode("\u00D7"));
-  li.appendChild(span);
-  // attachEventToDeleteItem(span);
-  span.addEventListener('click',deleteItemFromList);
-}
 
-//deleteItemAndSetCountValue
-function deleteItemFromList(){
-  var item = this.parentElement.id;
-  this.parentElement.remove();
-  deleteItemFromAnArray(item);
-  displayItemsCount(getItemsFromStorage().length);
-}
-//deleteItemFromArrayAndStorage
-function deleteItemFromAnArray(item){
-  var storageData = getItemsFromStorage();
-  var currentItem = storageData.find(function(array){
-    return array.id === Number(item);
-  });
-  storageData.splice(storageData.indexOf(currentItem),1);
-  addItemsToStorage(storageData);
-}
-//
-function inputFieldReset(inputText){
-  inputText.value = '';
-  inputText.focus();
-}
-//getAllItemsToRender
-function getAllItems(){
-  var selectedStorage = getStorageType();
-  if(selectedStorage === 'localStorage' || selectedStorage === 'sessionStorage'){
-    var storageData = getItemsFromStorage();
-    renderStorageData(storageData);
-  }
-}
-//getCompletedItemsToRender
-function getCompletedItems(){
-  var selectedStorage = getStorageType();
-  if(selectedStorage === 'localStorage' || selectedStorage === 'sessionStorage'){
-    var storageData = getItemsFromStorage();
-    renderItems(storageData,true);
-  }
-}
-//getPendingItemsToRender
-function getPendingItems(){
-  var selectedStorage = getStorageType();
-  if(selectedStorage === 'localStorage' || selectedStorage === 'sessionStorage'){
-    var storageData = getItemsFromStorage();
-    renderItems(storageData,false);
-  }
-}
-//
-function renderItems(storageData,itemStatus){
-  clearDisplayArea(document.getElementById('displayArea'));
-  var count = 0;
-  for(var i = 0; i < storageData.length; i++){
-    if(storageData[i].status === itemStatus){
-      createDisplayItemInstance(storageData[i].id,storageData[i].name,storageData[i].status);
-      count++;
+  this.tasksOnStorageType = function(){
+    var selectedStorage = self.getStorageType();
+    if(selectedStorage === 'localStorage' || selectedStorage === 'sessionStorage'){
+      var storageData = self.getItemsFromStorage();
+      self.displayStorageItems(storageData);
+    } else {
+      self.showMessageOnInvalidStorage();
+      self.displayItemsCount(0);
     }
   }
-  displayItemsCount(count);
-}
-//
-function displayItemsCount(count){
-  document.getElementById('myCount').innerHTML = count;
+
+  this.displayStorageItems = function(storageData){
+    self.clearTaskDisplayArea(document.getElementById('displayArea'));
+    for(var i = 0; i < storageData.length; i++){
+      createViewInstance().createItem(storageData[i].id,storageData[i].name,storageData[i].status);
+    }
+    self.displayItemsCount(storageData.length);
+  }
+
+  this.createStorageManagerInstance = function(storageType){
+    var selectedStorage = self.getStorageType();
+    if(selectedStorage === 'localStorage' || selectedStorage === 'sessionStorage'){
+      return new StorageManager(storageType,'myTodoItems');
+    }
+    else{
+      return '';
+    }
+  }
+
+  this.getItemsFromStorage = function(){
+    return self.createStorageManagerInstance('localStorage').getData();
+  }
+
+  this.clearTaskDisplayArea = function(displayAreaId){
+    displayAreaId.innerHTML = '';
+  }
+
+  this.getItemOnEnter = function(){
+    var enterKeyCode = 13;
+    var inputText = document.getElementById('myInput');
+    if (event.keyCode === enterKeyCode){
+      self.newItemTasks(inputText);
+    }
+  }
+
+  this.attachEventToAddTaskButton = function(button){
+    button.addEventListener('click', self.getItemOnAddButtonClick);
+  }
+
+  this.getItemOnAddButtonClick = function(){
+    var inputText = document.getElementById('myInput');
+    self.newItemTasks(inputText);
+  }
+  
+  this.newItemTasks = function(inputText){
+    var selectedStorage = self.getStorageType();  
+    if(inputText.value !== '' && (selectedStorage === 'localStorage' || selectedStorage === 'sessionStorage')){
+      var id = Date.now();
+      self.allItems();
+      self.addItemToAnArray(id,inputText.value);
+      createViewInstance().createItem(id,inputText.value);
+      self.inputFieldReset(inputText);
+      self.displayItemsCount(self.getItemsFromStorage().length);
+    }
+  }
+
+  this.addItemToAnArray = function(id,item){
+    var storageData = self.getItemsFromStorage();
+    storageData.push({id:id,name:item,status:false});
+    self.addItemsToStorage(storageData);
+  }
+
+  this.addItemsToStorage = function(storageData){
+    var selectedStorage = self.getStorageType();
+    if(selectedStorage === 'localStorage' || selectedStorage === 'sessionStorage'){
+      self.createStorageManagerInstance('localStorage').setData(storageData);
+      self.createStorageManagerInstance('sessionStorage').setData(storageData);
+    }
+  }
+
+  this.attachEventToCheckButton = function(checkButton){
+    checkButton.addEventListener('click',self.setItemStatus);
+  }
+
+  this.setItemStatus = function(){
+    var storageData = self.getItemsFromStorage();
+    var item = this.parentElement.id;
+    this.classList.toggle('checked');
+    var currentItem = storageData.find(function(array){
+        return array.id === Number(item);
+    });
+    self.setStatusValueToStorage(this.classList.value,storageData.indexOf(currentItem));
+  }
+
+  this.setStatusValueToStorage = function(classValue,itemIndex){
+    var storageData = self.getItemsFromStorage();
+    (classValue === '') ? storageData[itemIndex].status = false : storageData[itemIndex].status = true;
+    self.addItemsToStorage(storageData);
+  }
+
+  this.setItemClassValue = function(checkButton,status){
+    if(status === true){
+      checkButton.classList = 'checked';
+    }
+  }
+
+  this.attachEventToDeleteButton = function(deleteButton){
+    deleteButton.addEventListener('click',self.deleteItem);
+  }
+
+  this.deleteItem = function(){
+    var item = this.parentElement.id;
+    this.parentElement.remove();
+    self.deleteItemFromStorage(item);
+    self.displayItemsCount(self.getItemsFromStorage().length);
+  }
+
+  this.deleteItemFromStorage = function(item){
+    var storageData = self.getItemsFromStorage();
+    var currentItem = storageData.find(function(array){
+        return array.id === Number(item);
+    });
+    storageData.splice(storageData.indexOf(currentItem),1);
+    self.addItemsToStorage(storageData);
+  }
+
+  this.inputFieldReset = function(inputText){
+    inputText.value = '';
+    inputText.focus();
+  }
+  
+  this.attachEventToAllTaskButton = function(button){
+    button.addEventListener('click', self.allItems);
+  }
+  
+  this.attachEventToCompletedTaskButton = function(button){
+    button.addEventListener('click', self.completedItems);
+  }
+  
+  this.attachEventToPendingTaskButton = function(button){
+    button.addEventListener('click', self.pendingItems);  
+  }
+
+  this.allItems = function(){
+    var selectedStorage = self.getStorageType();
+    if(selectedStorage === 'localStorage' || selectedStorage === 'sessionStorage'){
+      var storageData = self.getItemsFromStorage();
+      self.displayStorageItems(storageData);
+    }
+  }
+
+  this.completedItems = function(){
+    var selectedStorage = self.getStorageType();
+    if(selectedStorage === 'localStorage' || selectedStorage === 'sessionStorage'){
+      self.clearTaskDisplayArea(document.getElementById('displayArea'));
+      var storageData = self.getItemsFromStorage();
+      self.displayItemsCount(self.displayItemsOnStatus(storageData,true));
+    }
+  }
+
+  this.pendingItems = function(){
+    var selectedStorage = self.getStorageType();
+    if(selectedStorage === 'localStorage' || selectedStorage === 'sessionStorage'){
+      self.clearTaskDisplayArea(document.getElementById('displayArea'));
+      var storageData = self.getItemsFromStorage();
+      self.displayItemsCount(self.displayItemsOnStatus(storageData,false));
+    }
+  }
+
+  this.displayItemsOnStatus = function(storageData,itemStatus){
+    var count = 0;
+    for(var i = 0; i < storageData.length; i++){
+      if(storageData[i].status === itemStatus){
+        createViewInstance().createItem(storageData[i].id,storageData[i].name,storageData[i].status);
+        count++;
+      }
+    }
+    return count;
+  }
+
+  this.displayItemsCount = function(count){
+    document.getElementById('myCount').innerHTML = count;
+  }
 }
--- view.js
+++ view.js
@@ -1,59 +1,93 @@
 
-function DisplayItem(id,item,status){
-  this.id = id;
-  this.item = item;
-  this.status = status;
-  this.createItem = function(){ createAnItem(this.id,this.item,this.status); }
+function createViewInstance(){
+  return new View();
 }
 
-function appendItemToList(item){
-  var displayAreaId = document.getElementById('displayArea');
-  displayAreaId.appendChild(item);
+createViewInstance().createAddTaskButton();
+createViewInstance().createAllTaskButton();
+createViewInstance().createCompletedTaskButton();
+createViewInstance().createPendingTaskButton();
+
+function View(){
+
+  var self = this;
+
+  this.createItem = function(id,item,status){
+    var li = document.createElement("li");
+    li.id = id;
+    self.createCheckButton(li,status);
+    self.createTextContent(li,item);
+    self.createDeleteButton(li);
+    self.appendItemToList(li);
+  }
+
+  this.createCheckButton = function(li,status){
+    var checkButton = document.createElement("SPAN");
+    checkButton.id = "check";
+    createControllerInstance().setItemClassValue(checkButton,status);
+    createControllerInstance().attachEventToCheckButton(checkButton);
+    li.appendChild(checkButton);
+  }
+
+  this.createTextContent = function(li,item){
+    var taskNode = document.createElement("SPAN");
+    taskNode.appendChild(document.createTextNode(item));
+    li.appendChild(taskNode);
+  }
+
+  this.createDeleteButton = function(li){
+    var deleteButton = document.createElement("SPAN");
+    deleteButton.className = "close";
+    deleteButton.appendChild(document.createTextNode("\u00D7"));
+    li.appendChild(deleteButton);
+    createControllerInstance().attachEventToDeleteButton(deleteButton);
+  }
+
+  this.appendItemToList = function(item){
+    var displayAreaId = document.getElementById('displayArea');
+    displayAreaId.appendChild(item);
+  }
+
+  this.appendAddButton = function(button){
+    var headerId = document.getElementById('header');
+    headerId.appendChild(button);
+  }
+
+  this.appendTaskButtons = function(button){
+    var footerId = document.getElementById('footer');
+    footerId.appendChild(button);
+  }
+
+  this.createAddTaskButton = function(){
+    var button = document.createElement("BUTTON");
+    button.id = 'addButton';
+    button.appendChild(document.createTextNode("Add"));
+    self.appendAddButton(button);
+    createControllerInstance().attachEventToAddTaskButton(button);
+  }
+
+  this.createAllTaskButton = function(){
+    var button = document.createElement("BUTTON");
+    button.id = 'allTaskButton';
+    button.appendChild(document.createTextNode("All Tasks"));
+    self.appendTaskButtons(button);
+    createControllerInstance().attachEventToAllTaskButton(button);
+  }
+
+  this.createCompletedTaskButton = function(){
+    var button = document.createElement("BUTTON");
+    button.id = 'completedTaskButton';
+    button.appendChild(document.createTextNode("Completed"));
+    self.appendTaskButtons(button);
+    createControllerInstance().attachEventToCompletedTaskButton(button);
+  }
+
+  this.createPendingTaskButton = function(){
+    var button = document.createElement("BUTTON");
+    button.id = 'pendingTaskButton';
+    button.appendChild(document.createTextNode("Pending"));
+    self.appendTaskButtons(button);
+    createControllerInstance().attachEventToPendingTaskButton(button);
+  }
 }
 
-function appendAddButton(button){
-  var headerId = document.getElementById('header');
-  headerId.appendChild(button);
-}
-
-function appendTaskButtons(button){
-  var footerId = document.getElementById('footer');
-  footerId.appendChild(button);
-}
-
-//createAddButtonAndSetEvent
-function createAddTaskButton(){
-  var button = document.createElement("BUTTON");
-  button.id = 'addButton';
-  button.appendChild(document.createTextNode("Add"));
-  appendAddButton(button);
-  var instance = new Controller();
-  button.addEventListener('click', instance.getItemOnAddButtonClick);
-}
-//createAllTaskButtonAndSetEvent
-function createAllTaskButton(){
-  var button = document.createElement("BUTTON");
-  button.id = 'allTaskButton';
-  button.appendChild(document.createTextNode("All Tasks"));
-  appendTaskButtons(button);
-  var instance = new Controller();
-  button.addEventListener('click', instance.getAllItems);
-}
-//createCompletedButtonAndSetEvent
-function createCompletedTaskButton(){
-  var button = document.createElement("BUTTON");
-  button.id = 'completedTaskButton';
-  button.appendChild(document.createTextNode("Completed"));
-  appendTaskButtons(button);
-  var instance = new Controller();
-  button.addEventListener('click', instance.getCompletedItems);
-}
-//createPendingButtonAndSetEvent
-function createPendingTaskButton(){
-  var button = document.createElement("BUTTON");
-  button.id = 'pendingTaskButton';
-  button.appendChild(document.createTextNode("Pending"));
-  appendTaskButtons(button);
-  var instance = new Controller();
-  button.addEventListener('click', instance.getPendingItems);
-}
